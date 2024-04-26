0a0ed358ff6a4c0f7976d9efdf8201bfa259c5eb
Christian Manzaraz <magnusmanz@gmail.com>
2024-04-26 18:48:24 -0300

    Section 6 Improve your app design Now that the app is functional, add your own colors and icons to make it unique.

--- Pick-a-Pal/Views/ContentView.swift
+++ Pick-a-Pal/Views/ContentView.swift
@@ -8,19 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var names: [String] = ["Elisha", "Andre", "Jasmine", "Po-Chun"]
+    @State private var names: [String] = []
     @State private var nameToAdd = ""
     @State private var pickedName = ""
     @State private var shouldRemovePickedName = false
     
     var body: some View {
         VStack {
+            VStack {
+                Image(systemName: "person.3.sequence.fill")
+                    .foregroundStyle(.tint)
+                    .symbolRenderingMode(.hierarchical)
+                
+                Text("Pick-a-Pall")
+                
+            }
+            .font(.title)
+            .bold()
+            
             Text(pickedName.isEmpty ? "" : pickedName)
+                .font(.title2)
+                .bold()
+                .foregroundStyle(.tint)
+            
             List {
                 ForEach(names, id: \.self) { name in
                     Text(name)
                 }
             }
+            .clipShape(RoundedRectangle(cornerRadius: 8))
             
             TextField("Add a Name", text: $nameToAdd)
                 .autocorrectionDisabled()
